2501f55b1009346aa281e5f4d473e1f92b3a2ce5
K0tlyarski <djgame2.9.6@gmail.com>
2024-03-20 15:06:51 +0400

    test

---
+++ lab1/lab1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by Вадим Котлярский on 20.03.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
